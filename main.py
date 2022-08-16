656a7ead7ce784f1b8525fb49797d84d4191186a
Sayantan Mukhopadhyay <59133918+sayantanmukh050893@users.noreply.github.com>
2022-08-17 00:26:21 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,86 @@
+from __future__ import division
+import cv2
+import streamlit as st
+from PIL import Image
+from keras.models import Sequential
+from keras.layers import Dense
+from keras.models import model_from_json
+import numpy
+import os
+import numpy as np
+
+#loading the model
+json_file = open('model.json', 'r')
+loaded_model_json = json_file.read()
+json_file.close()
+loaded_model = model_from_json(loaded_model_json)
+# load weights into new model
+loaded_model.load_weights("weights.h5")
+
+WIDTH = 48
+HEIGHT = 48
+x=None
+y=None
+labels = ['Angry', 'Disgust', 'Fear', 'Happy', 'Sad', 'Surprise', 'Neutral']
+
+st.title("Webcam Live Feed")
+image = Image.open('human_sentiments.jpg')
+st.image(image)
+
+# button = 
+# analyze = 
+# #disable_button = st.button("Disable Camera")
+run = st.checkbox('Enable Webcam')
+FRAME_WINDOW = st.image([])
+camera = cv2.VideoCapture(0)
+
+while run:
+    _, frame = camera.read()
+    frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+    gray = cv2.cvtColor(frame, cv2.COLOR_RGB2GRAY)
+    face = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+    faces = face.detectMultiScale(
+    gray,
+    scaleFactor=1.1,
+    minNeighbors=5,
+    minSize=(48, 48)
+    # ,
+    # flags=cv2.CV_HAAR_SCALE_IMAGE
+)
+    for (x, y, w, h) in faces:
+        #cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 2)
+        roi_gray = gray[y:y + h, x:x + w]
+        cropped_img = np.expand_dims(np.expand_dims(cv2.resize(roi_gray, (48, 48)), -1), 0)
+        cv2.normalize(cropped_img, cropped_img, alpha=0, beta=1, norm_type=cv2.NORM_L2, dtype=cv2.CV_32F)
+        cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 1)
+        yhat= loaded_model.predict(cropped_img)
+        cv2.putText(frame, labels[int(np.argmax(yhat))], (x, y), cv2.FONT_HERSHEY_SIMPLEX, 0.8, (0, 255, 0), 1, cv2.LINE_AA)
+    cv2.imshow('Video', frame)
+    FRAME_WINDOW.image(frame)
+else:
+    st.write('')
+
+# if st.checkbox('Run'):
+#     FRAME_WINDOW = st.image([])
+#     camera = cv2.VideoCapture(0)
+
+#     if st.button("Analyze Face"):
+#         st.write('Analyze')
+#         _, frame = camera.read()
+#         #frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+#         frame = cv2.cvtColor(frame, cv2.COLOR_RGB2GRAY)
+#         face = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+#         faces = face.detectMultiScale(
+#         frame,
+#         scaleFactor=1.1,
+#         minNeighbors=5,
+#         minSize=(30, 30),
+#         flags=cv2.cv.CV_HAAR_SCALE_IMAGE
+#     )
+#         for (x, y, w, h) in faces:
+#             print(x,y,w,h)
+#             cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 2)
+#         cv2.imshow('Video', frame)
+#         FRAME_WINDOW.image(frame)
+# else:
+#     st.write('')
